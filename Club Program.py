b5da78cc30a9535ea7d8ee7e0ef0bda8532b1084
fayha'a alharbi <61951032+fyoalharbi@users.noreply.github.com>
2024-10-01 00:03:07 +0300

    Update Club Program.py
    Not used in the Dashboard

--- Club Program.py
+++ Club Program.py
@@ -2,16 +2,13 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import numpy as np
-# Step 1: Define the URL of the webpage
 url = 'http://127.0.0.1:5500/Esports%20World%20Cup%202024%20-%20Liquipedia%20Esports%20Wiki%20-%20main%20page.htm'
 
-# Step 2: Fetch the content of the webpage
 response = requests.get(url)
 soup = BeautifulSoup(response.content, 'html.parser')
 
 table = soup.find('table', class_='sortable wikitable2')
 
-# Step 4: Initialize lists to store the scraped data
 Country = []
 Headquarters = []
 team = []
@@ -19,19 +16,17 @@ Owner = []
 b42024 = []
 in2024 = []
 En2024 = [] 
-# Step 5: Iterate over each row in the table and extract the data
 for row in table.find('tbody').find_all('tr'):
     c = row.find('th').find('span')
     Country.append(c.find('a')['title']) if c else None 
     cells = row.find_all('td')
     Headquarters.append(row.find('th').text.strip())
     if len(cells) > 0:
-        # Extract text from each cell and append to respective lists
         #team name
         t = cells[0].find('span')
         team.append(t.find('span', class_='team-template-text').find('a')['title']) if t else None  # Date
         
-        Owner.append(cells[1].text.strip())  # Prize Pool
+        Owner.append(cells[1].text.strip())  # prize 
 
         a = cells[2].find_all('img')
         a = ', '.join(link['alt'] for link in a)
@@ -46,7 +41,6 @@ for row in table.find('tbody').find_all('tr'):
         En2024.append(a) if a else En2024.append('None')
 Headquarters = Headquarters[2:]
 
-# Step 6: Create a DataFrame to organize the data
 df_tournaments = pd.DataFrame({
     'Country': Country,
     'Headquarters': Headquarters,
@@ -57,7 +51,6 @@ df_tournaments = pd.DataFrame({
     'entered in 2024': En2024
 })
 
-# Step 7: Save the DataFrame to an Excel or CSV file
 output_file = 'club_organization_2024.xlsx'
 df_tournaments.to_excel(output_file, index=False)
 

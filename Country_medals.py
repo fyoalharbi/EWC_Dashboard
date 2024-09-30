147e0581e23164971b47e8b85fa0400794280662
fayha'a alharbi <61951032+fyoalharbi@users.noreply.github.com>
2024-10-01 00:03:42 +0300

    Update Country_medals.py

--- Country_medals.py
+++ Country_medals.py
@@ -2,23 +2,19 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-# Step 1: Define the URL of the webpage
 url = 'http://127.0.0.1:5500/Esports%20World%20Cup%202024%20-%20Liquipedia%20Esports%20Wiki%20-%20main%20page.htm'
 
-# Step 2: Fetch the content of the webpage
 response = requests.get(url)
 soup = BeautifulSoup(response.content, 'html.parser')
 
 table = soup.find('table', class_='wikitable plainrowheaders sortable')
 
-# Step 4: Initialize lists to store the scraped data
 country = []
 first = []
 second = []
 third = []
 total = []
 
-# Step 5: Iterate over each row in the table and extract the data
 for row in table.find('tbody').find_all('tr'):
     
     c = row.find('th').find('span')
@@ -28,12 +24,10 @@ for row in table.find('tbody').find_all('tr'):
     cells = row.find_all('td')#.find('b')
 
     if len(cells) > 0:
-        # Extract text from each cell and append to respective lists
-        second.append(cells[1].text.strip())  # Event
-        third.append(cells[2].text.strip())  # Prize Pool
-        total.append(cells[3].text.strip())  # Participants
+        second.append(cells[1].text.strip())  
+        third.append(cells[2].text.strip())  
+        total.append(cells[3].text.strip())  
 first = first[1:]
-# Step 6: Create a DataFrame to organize the data
 df_tournaments = pd.DataFrame({
     'country': country,
     'first': first,
@@ -42,7 +36,6 @@ df_tournaments = pd.DataFrame({
     'total': total,
 })
 
-# Step 7: Save the DataFrame to an Excel or CSV file
 output_file = 'country_medals.xlsx'
 df_tournaments.to_excel(output_file, index=False)
 
